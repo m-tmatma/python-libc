4563501bec9c3b3b14123ac5a0eb6bcef6e70399
Masaru Tsuchiyama <m.tmatma@gmail.com>
2022-02-14 07:53:50 +0900

    add nonblocking test.

--- tests/test_libc.py
+++ tests/test_libc.py
@@ -1,6 +1,7 @@
 import errno
 import os
 import sys
+import select
 import time
 import pytest
 
@@ -34,6 +35,41 @@ def test_timerfd():
     assert exc_info.value.args[1] == os.strerror(errno.EBADF)
 
 
+def test_timerfd_nonblocking():
+    tfd = timerfd_create(CLOCK.REALTIME, TFD.NONBLOCK)
+
+    ev = select.epoll()
+    ev.register(tfd, select.EPOLLIN)
+
+    timerfd_settime(tfd, 0, (0.5, 1))
+
+    t = time.perf_counter()
+    for i in range(3):
+        events = ev.poll(-1)
+        for fd, event in events:
+            if event & select.EPOLLIN:
+                if fd == tfd:
+                    print("signaled")
+                    _ = os.read(tfd, 8)
+    t = time.perf_counter() - t
+    assert 2 - 1e-3 < t < 2 + 1e-3
+
+    ev.unregister(tfd)
+
+    # close timerfd
+    os.close(tfd)
+
+    # try to close the timerfd which was already closed.
+    with pytest.raises(OSError) as exc_info:
+        os.close(tfd)
+
+    # check detail of OSError
+    assert exc_info.value.args[0] == errno.EBADF
+    assert exc_info.value.args[1] == os.strerror(errno.EBADF)
+
+    del ev
+
+
 def test_eventfd():
     efd = eventfd(0, 0)
 
@@ -53,3 +89,36 @@ def test_eventfd():
     # check detail of OSError
     assert exc_info.value.args[0] == errno.EBADF
     assert exc_info.value.args[1] == os.strerror(errno.EBADF)
+
+def test_eventfd_nonblocking():
+    efd = eventfd(0, EFD.NONBLOCK)
+
+    ev = select.epoll()
+    ev.register(efd, select.EPOLLIN)
+
+    a = 10
+    _ = os.write(efd, a.to_bytes(8, byteorder=sys.byteorder))
+
+    events = ev.poll(-1)
+    for fd, event in events:
+        if event & select.EPOLLIN:
+            if fd == efd:
+                print("signaled")
+                b = os.read(efd, 8)
+                ev.unregister(efd)
+
+                c = int.from_bytes(b, byteorder=sys.byteorder)
+                assert a == c
+
+    os.close(efd)
+
+    # try to close the fd which was already closed.
+    with pytest.raises(OSError) as exc_info:
+        os.close(efd)
+
+    # check detail of OSError
+    assert exc_info.value.args[0] == errno.EBADF
+    assert exc_info.value.args[1] == os.strerror(errno.EBADF)
+
+    del ev
+
